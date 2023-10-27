4a05f42720f29bff61445bb5c93fbb9f91e8084a
sangjun <sjun8932.3@gmail.com>
2023-10-27 12:48:06 +0900

    003.example React 세상

--- Frontend/class/pages/index.js
+++ Frontend/class/pages/index.js
@@ -1,7 +1,8 @@
+import styles from '../styles/Home.module.css'
 export default function Home() {
     return(
         <>
-            <h1>몰입해서 해봅시다.</h1>
+            <h1 className={styles.lesserafim}>몰입해서 해봅시다.</h1>
         </>
     )
 }
---
+++ Frontend/class/pages/section01/01-01-example/index.js
@@ -0,0 +1,12 @@
+export default function 나만의페이지() {
+
+    const aaa = 3; // 위쪽은 자바스크립트 작성하는 공간
+
+    // 아래쪽은 html 작성하는 공간
+    return(
+        <div>
+            <div>김채원~!!</div>
+            <button>내 동료가 되는 버튼이야~</button>
+        </div>
+    )
+}
