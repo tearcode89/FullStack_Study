3a276f92e67bdf33201d06e62caa0e25259431b6
sangjun <sjun8932.3@gmail.com>
2023-10-30 12:04:26 +0900

    005.포트폴리오 리뷰

---
+++ Frontend/portfolio/section01/styles/boardsNew.js
@@ -0,0 +1,139 @@
+import styled from "@emotion/styled";
+
+export const Address = styled.div`
+  width: 996px;
+  height: 52px;
+  margin-top: 16px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-top: 80px;
+`
+
+export const Contents = styled.textarea`
+  width: 996px;
+  height: 480px;
+  padding-left: 16px;
+  padding: 14px;
+  border: 1px solid #bdbdbd;
+`
+export const ImageWrapper = styled.div`
+  width: 996px;
+  padding-top: 40px;
+`
+export const InputWrapper = styled.div`
+  padding-top: 40px;
+`
+export const Label = styled.div`
+  padding-bottom: 16px;
+  font-size: 16px;
+  font-weight: 500;
+`
+export const OptionWrapper = styled.div`
+  width: 996px;
+  padding-top: 40px;
+`
+export const Password = styled.input`
+  width: 486px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`
+export const RadioButton = styled.input`
+  cursor: pointer;
+`
+export const RadioLabel = styled.label`
+  margin-left: 8px;
+  margin-right: 20px;
+  font-weight: 500;
+  cursor: pointer;
+`
+export const SearchButton = styled.button`
+  width: 124px;
+  height: 52px;
+  margin-left: 16px;
+  background-color: black;
+  cursor: pointer;
+  color: white;
+`
+export const Subject = styled.input`
+  width: 996px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`
+export const SubmitButton = styled.button`
+  width: 179px;
+  height: 52px;
+  border: none;
+  font-size: 16px;
+  font-weight: 500;
+  margin-left: 12px;
+  margin-right: 12px;
+  cursor: pointer;
+  background-color: yellow;
+`
+export const Title = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 34px;
+  font-weight: bold;
+`
+export const UploadButton =  styled.button`
+  width: 78px;
+  height: 78px;
+  background-color: #bdbdbd;
+  margin-right: 24px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+`
+export const Wrapper = styled.div`
+  width: 1200px;
+  // height: 1847px;
+  border: 1px solid black;
+  margin: 100px;
+  padding-top: 80px;
+  padding-bottom: 100px;
+  padding-left: 102px;
+  padding-right: 102px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: none;
+  box-shadow: 0px 0px 10px grey;
+  // ackground-color: red;b
+`
+export const Writer = styled.input`
+  width: 486px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`
+export const WriterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 40px;
+`
+export const Youtube = styled.input`
+  width: 996px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`
+export const Zipcode = styled.input`
+  width: 77px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`
+export const ZipcodeWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
