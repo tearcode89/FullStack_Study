5ac3e42e61eb9f4bf1df7601164a27fbcd6169f0
sangjun <sjun8932.3@gmail.com>
2023-10-31 18:09:10 +0900

    020.static-routing-board

---
+++ Frontend/class/pages/section05/05-02-static-routing-board-moved/1/index.js
@@ -0,0 +1,8 @@
+export default function StaticRoutingMovedPage(){
+
+    return(
+        <>
+            <div>1번 게시글 이동이 완료됐어요!!😀😀</div>
+        </>
+    )
+}
---
+++ Frontend/class/pages/section05/05-02-static-routing-board-moved/2/index.js
@@ -0,0 +1,8 @@
+export default function StaticRoutingMovedPage(){
+
+    return(
+        <>
+            <div>2번 게시글 이동이 완료됐어요!!😀😀</div>
+        </>
+    )
+}
