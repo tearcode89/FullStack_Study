c3be659589e8fa2f4418fe80999661f63b069170
sangjun <sjun8932.3@gmail.com>
2023-10-31 10:58:04 +0900

    013.GraphQL 세팅

--- Frontend/class/pages/section04/04-01-rest-get/index.js
+++ Frontend/class/pages/section04/04-01-rest-get/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import 나만의페이지 from '../../section01/01-01-example'
 
 export default function RestGetPage() {
 
@@ -24,6 +25,7 @@ export default function RestGetPage() {
         <div>
             <button onClick={onClickAsync}>REST-API(비동기) 요청하기</button>
             <button onClick={onClickSync}>REST-API(동기) 요청하기</button>
+            <나만의페이지/>
         </div>
     )
 }
