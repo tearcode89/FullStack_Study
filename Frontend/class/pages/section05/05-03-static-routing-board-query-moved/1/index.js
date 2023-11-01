e80c4184f36b7787dcfe07cc62cdeb8d87832f82
sangjun <sjun8932.3@gmail.com>
2023-11-01 09:49:57 +0900

    021.static-routing-board-query

---
+++ Frontend/class/pages/section05/05-03-static-routing-board-query-moved/1/index.js
@@ -0,0 +1,27 @@
+import {gql, useQuery} from "@apollo/client";
+
+const FETCH_BOARD = gql`
+    query{
+        fetchBoard(number:19051){ # 1번 게시글 내용에 생략된 내용이 많아서 19047을 1번 게시글로 가정
+            number
+            writer
+            title
+        contents
+    }
+}
+`
+export default function StaticRoutingMovedPage(){
+
+    const { data } = useQuery(FETCH_BOARD)
+
+    console.log(data)
+
+    return(
+        <>
+            <div>1번 게시글 이동이 완료됐어요!!😀😀</div>
+            <div>작성자: {data && data.fetchBoard?.writer}</div>
+            <div>제목: {data?.fetchBoard?.title}</div>
+            <div>내용: {data? data.fetchBoard?.contents : "로딩중입니다."}</div>
+        </>
+    )
+}
