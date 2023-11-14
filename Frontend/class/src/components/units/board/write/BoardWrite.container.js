cdfe1ad7a9ede61d8987b1f317c73136fd9930b1
sangjun <sjun8932.3@gmail.com>
2023-11-14 10:31:52 +0900

    026.export vs export-default

--- Frontend/class/src/components/units/board/write/BoardWrite.container.js
+++ Frontend/class/src/components/units/board/write/BoardWrite.container.js
@@ -1,7 +1,13 @@
 import {useMutation} from "@apollo/client";
 import {useState} from "react";
-import BoardWriteUI from "./BoardWrite.presenter";
-import {나의그래프큐엘세팅} from "./BoardWrite.queries";
+import {나의그래프큐엘세팅} from "./BoardWrite.queries"; // export는 골라서 가져오기 가능
+import BoardWriteUI from "./BoardWrite.presenter"; // export default로 한개만 가져오기
+// import asdfljlkenvllle from "./BoardWrite.presenter"; // export default로 이름 변경해서 가져요기
+// import asdfljlkenvllle, {나의그래프큐엘세} from "./BoardWrite.presenter"; // export default와 export로 되어있는 것들을 한번에 가져오기
+// import * as QQQ from './BoardWrite.styles' // export를 한방에 다 가져오기
+// QQQ.BlueButton
+// QQQ.RedInput
+
 export default function BoardWrite() {
 
     const [나의함수] = useMutation(나의그래프큐엘세팅)
