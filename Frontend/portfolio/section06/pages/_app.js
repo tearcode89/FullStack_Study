e2dfe1145574f7f21466ca8a36b43e282ffccede
sangjun <sjun8932.3@gmail.com>
2023-11-15 12:29:16 +0900

    027.포트폴리오 리뷰

---
+++ Frontend/portfolio/section06/pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/global.css'
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+export default function App({ Component, pageProps }) {
+
+    const client = new ApolloClient({
+        uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+        cache: new InMemoryCache()
+    })
+
+    return (
+        <>
+            <ApolloProvider client={client}>
+                <Component {...pageProps} />
+            </ApolloProvider>
+        </>
+    )
+}
